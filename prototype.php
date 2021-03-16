bf756b0797a651402f5ae423e991b1324e2da351
arvianaoktavianty14 <80742956+arvianaoktavianty14@users.noreply.github.com>
2021-03-16 15:26:04 +0700

    Add files via upload

---
+++ prototype.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>SIPDA | Sistem Pemilihan Umum Daring</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="bower_components/morris.js/morris.css">
+  <link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">
+  <link rel="stylesheet" href="bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+  <link rel="stylesheet" href="bower_components/bootstrap-daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+  <link rel="stylesheet" href="dist/css/css.css">
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+ 
+<div style="font-family: 'Poppins', sans-serif;">
+
+<div class="card-block-content-2-3">
+  <div class="card-content-2-3-atas">
+    <div class="d-flex flex-lg-row flex-column align-items-center">
+      <!-- <div class="me-lg-3">
+        <img src="http://api.elements.buildwithangga.com/storage/files/2/assets/Content/Content2/Content-2-1%20(1).png" alt="">          
+      </div> -->
+      <div class="flex-grow-1 text-lg-start text-center card-text-content-2-3">
+        <h3 class="card-content-2-3-title-atas">SIPDA-REMA UPI</h3>
+        <p class="d-none d-lg-block card-content-2-3-caption-atas">
+        Sistem Pemilihan Umum Daring (SIPDA) REMA UPI merupakan program aplikasi pemungutan suara Pemilihan Umum calon presiden dan wakil
+        presiden BEM REMA UPI berbasis online bagi mahasiswa D3 dan S1 di lingkungan Universitas Pendidikan Indonesia (UPI) yang bekerjasama
+        dengan Direktorat Sistem Informasi UPI. Sistem pemungutan suara pemilu REMA UPI telah terintegrasi dengan sistem aplikasi yang sudah stabil
+        dimiliki oleh UPI saat ini yaitu Sistem Single Sign On (SSO)
+      </p>
+      
+      </div>
+      <!-- <div class="card-btn-space-content-2-3">
+        <button class="btn btn-card-content-2-3">Login</button>
+      </div> -->
+    </div>
+    
+  </div>
+</div>
+
+<div class="card-block-content-2-3">
+  <div class="card-content-2-3-bawah">
+    <div class="d-flex flex-lg-row flex-column align-items-center">
+      <!-- <div class="me-lg-3">
+        <img src="http://api.elements.buildwithangga.com/storage/files/2/assets/Content/Content2/Content-2-1%20(1).png" alt="">          
+      </div> -->
+      <div class="flex-grow-1 text-lg-start text-center card-text-content-2-3">
+        <h3 class="card-content-2-3-title-bawah">Layanan Pemilu REMA UPI 2021</h3>
+        <p class="d-none d-lg-block card-content-2-3-caption-bawah">kpurema@upi.edu</p>
+        <p class="d-none d-lg-block card-content-2-3-caption-bawah">@kpu.remaupi</p>
+        <p class="d-none d-lg-block card-content-2-3-caption-bawah">081283469189</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+
+</section> 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+ 
+  <div class="control-sidebar-bg"></div>
+</div>
+
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/jquery-ui/jquery-ui.min.js"></script>
+<script>
+  $.widget.bridge('uibutton', $.ui.button);
+</script>
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="bower_components/raphael/raphael.min.js"></script>
+<script src="bower_components/morris.js/morris.min.js"></script>
+<script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
+<script src="bower_components/moment/min/moment.min.js"></script>
+<script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="bower_components/fastclick/lib/fastclick.js"></script>
+<script src="dist/js/adminlte.min.js"></script>
+<script src="dist/js/pages/dashboard.js"></script>
+<script src="dist/js/demo.js"></script>
+</body>
+</html>
